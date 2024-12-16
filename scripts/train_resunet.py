e927f09a04396ed870300396888ea773ac15c4f0
Mikołaj Czachorowski <122210130+Micz26@users.noreply.github.com>
2024-12-17 00:18:49 +0100

    Update train_resunet.py

--- scripts/train_resunet.py
+++ scripts/train_resunet.py
@@ -6,16 +6,21 @@ from trashcan_core.components.constants import MATERIAL_VERSION_PATH
 from trashcan.components.models import ResUNet
 
 
-data_loader = DataLoader(MATERIAL_VERSION_PATH)
+def main():
+    data_loader = DataLoader(MATERIAL_VERSION_PATH)
 
-model = ResUNet()
+    model = ResUNet()
 
-loss = DiceLoss()
+    loss = DiceLoss()
 
-optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+    optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
 
-trainer = model.as_trainer(loss=loss, optimizer=optimizer, data_loader=data_loader)
+    trainer = model.as_trainer(loss=loss, optimizer=optimizer, data_loader=data_loader)
 
-trainer(20)
+    trainer(20)
 
-trainer.save_model("models/resunet.onnx", input_shape=(1, 3, 256, 480))
+    trainer.save_model("models/resunet.onnx", input_shape=(1, 3, 256, 480))
+
+
+if __name__ == "__main__":
+    main()
